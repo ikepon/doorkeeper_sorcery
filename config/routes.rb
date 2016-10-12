1f5d94417c564d4241b353e5a6e1f0c58d8a403a
ikepon <tatsuyanoheya@gmail.com>
2016-10-12 16:26:26 +0900

    Userのroutes, controller を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/create'
-
-  get 'users/show'
-
   use_doorkeeper
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:create, :show]
 end
